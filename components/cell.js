c3a6e43d0f1d42d89a7c5d1751f739e7580f4968
zisgeim 09 <ami145388@gmail.com>
2022-09-25 19:42:55 +0600

    third

--- components/cell.js
+++ components/cell.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Cell({ img, name=img, link }) {
     return (
         <div onClick={() => window.open(link, "_blank")} className='box'>
-            <Image src={`/../public/img/${img}`} alt={img} width={100} height={100} />
+            <Image src={`/../public/img/${img}`} alt={name} width={100} height={100} />
             <p className='name'>{name}</p>
         </div>
     );
